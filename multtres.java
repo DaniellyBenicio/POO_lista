bc5d3845350f273204953082d76cbe523f877c8e
DaniellyBenicio <115299113+DaniellyBenicio@users.noreply.github.com>
2023-11-13 17:38:53 -0300

    multtres.java

--- multtres.java
+++ multtres.java
@@ -2,7 +2,7 @@
 
 public class multtres {
     public static void main(String[] args) {
-        for (int i = 1; i < 100; i+=3){
+        for (int i = 1; i < 100; i++){
             if (i % 3 == 0){
                 System.out.println(i);
             }
